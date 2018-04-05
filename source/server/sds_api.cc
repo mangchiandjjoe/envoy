33efbe9934eebfd6c3345cd9fb07e76a2051ba89
Jae Kim <jaebong@google.com>
2018-04-05 09:55:48 -0700

    Changed TransportSocketFactoryPtr to use std::share_ptr

--- source/server/sds_api.cc
+++ source/server/sds_api.cc
@@ -13,14 +13,13 @@
 
 #include "envoy/ssl/secret.h"
 
-#include "server/sds_subscription.h"
 #include "envoy/server/secret_manager.h"
+#include "server/sds_subscription.h"
 
 namespace Envoy {
 namespace Server {
 
-SdsApi::SdsApi(Instance& server,
-               const envoy::api::v2::core::ConfigSource& sds_config,
+SdsApi::SdsApi(Instance& server, const envoy::api::v2::core::ConfigSource& sds_config,
                Envoy::Server::SecretManager& secret_manager)
     : server_(server),
       sds_config_([&sds_config] {
@@ -29,33 +28,25 @@ SdsApi::SdsApi(Instance& server,
         return cfg;
       }()),
       secret_manager_(secret_manager) {
-
   server_.initManager().registerTarget(*this);
 }
 
 void SdsApi::initialize(std::function<void()> callback) {
   initialize_callback_ = callback;
 
-  subscription_ =
-      Envoy::Config::SubscriptionFactory::subscriptionFromConfigSource<
-        envoy::api::v2::auth::Secret>(
-          sds_config_,
-          server_.localInfo().node(),
-          server_.dispatcher(),
-          server_.clusterManager(),
-          server_.random(),
-          server_.stats(),
-          [this]() -> Config::Subscription<envoy::api::v2::auth::Secret>* {
-            return new SdsSubscription(
-                Config::Utility::generateStats(this->server_.stats()),
-                this->sds_config_,
-                this->server_.clusterManager(),
-                this->server_.dispatcher(),
-                this->server_.random(),
-                this->server_.localInfo());
-          },
-          "envoy.service.discovery.v2.SecretDiscoveryService.FetchSecrets",
-          "envoy.service.discovery.v2.SecretDiscoveryService.FetchSecrets");
+  subscription_ = Envoy::Config::SubscriptionFactory::subscriptionFromConfigSource<
+      envoy::api::v2::auth::Secret>(
+      sds_config_, server_.localInfo().node(), server_.dispatcher(), server_.clusterManager(),
+      server_.random(), server_.stats(),
+      [this]() -> Config::Subscription<envoy::api::v2::auth::Secret>* {
+        return new SdsSubscription(
+            Config::Utility::generateStats(this->server_.stats()),
+            this->sds_config_, this->server_.clusterManager(),
+            this->server_.dispatcher(), this->server_.random(),
+            this->server_.localInfo());
+      },
+      "envoy.service.discovery.v2.SecretDiscoveryService.FetchSecrets",
+      "envoy.service.discovery.v2.SecretDiscoveryService.FetchSecrets");
 
   Config::Utility::checkLocalInfo("sds", server_.localInfo());
   subscription_->start({}, *this);
@@ -79,8 +70,7 @@ void SdsApi::onConfigUpdate(const ResourceVector& resources) {
 }
 
 void SdsApi::onConfigUpdateFailed(const EnvoyException*) {
-// We need to allow server startup to continue, even if we have a bad
-// config.
+  // We need to allow server startup to continue, even if we have a bad config.
   runInitializeCallbackIfAny();
 }
 
